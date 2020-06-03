6480f521f34e1ee4a75c94ebca4ca28c8ab39592
ycourlee <599113167+ycourlee@users.noreply.github.com>
2020-06-03 16:00:34 +0800

    添加food service

---
+++ mall-admin/src/main/java/com/commerce/mall/service/TmsFoodService.java
@@ -0,0 +1,17 @@
+package com.commerce.mall.service;
+
+import com.commerce.mall.model.TmsFood;
+
+/**
+ * @author jy
+ * @date 2020/6/3
+ */
+public interface TmsFoodService {
+
+    /**
+     * 添加食品
+     *
+     * @param tmsFood tmsFood
+     */
+    void add(TmsFood tmsFood);
+}
